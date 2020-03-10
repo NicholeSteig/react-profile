370af2c034bd5921437faf1f535854afa6a780e3
NicholeSteig <nichole.steig@gmail.com>
2020-03-09 18:27:37 -0700

    basic app and some components

---
+++ app.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Home from "./pages/home";
+import Portfolio from "./pages/portfolio";
+import Contact from "./pages/contact";
+
+function App() {
+    return (
+        <Router>
+            <div>
+                
+            </div>
+        </Router>
+    )
+}
---
+++ components/headshot/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+<Container>
+  <Row>
+    <Col xs={6} md={4}>
+      <Image src="holder.js/171x180" roundedCircle />
+    </Col>
+  </Row>
+</Container>
